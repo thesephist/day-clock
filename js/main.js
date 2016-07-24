ffac896eb5c5817e3ad57af80a1644c8050e22bb
Linus Lee <linus@thelifelongtraveler.com>
2016-07-24 15:23:27 -0400

    More style and script tweaks to get to MVP

--- js/main.js
+++ js/main.js
@@ -6,14 +6,17 @@ var hourHand = document.getElementById("hour"),
     dateBox = document.getElementById("date-readout"),
     bg = document.getElementById("backdrop");
 
+var clockAnimation,
+    cancelAnimation;
+
 // update function
 function updateClock() {
-    var time = new Date().getTime();
+    var time = new Date();
 
     // get times
-    var hour = time.getHours();
-    var minute = time.getMinutes();
-    var second = time.getSeconds();
+    var hour = time.getHours() + time.getMinutes() / 60;
+    var minute = time.getMinutes() + time.getSeconds() / 60;
+    var second = time.getSeconds() + time.getMilliseconds() / 1000;
 
     // get date
     var date = time.getDate();
@@ -24,29 +27,35 @@ function updateClock() {
 
     // render stuff
     applyAngle(hourHand, getAngle(hour, 12));
-    applyangle(minuteHand, getAngle(minute, 60));
+    applyAngle(minuteHand, getAngle(minute, 60));
     applyAngle(secondHand, getAngle(second, 60));
     dateBox.innerText = moment(time).format("MMM DD[,] YYYY");
 
     // draw gradient map occasionally; maybe like every minute
     // TODO
 
-    // RAF is bae
-    requestAnimationFrame(updateClock)
+    // rAF is bae
+    clockAnimatio = requestAnimationFrame(updateClock);
+
+    // for debugging
+    if (cancelAnimation) {
+        cancelAnimationFrame(clockAnimation);
+        clockAnimation = undefined;
+    }
 }
 
 function applyAngle(el, angleString) {
     if (el.style.transform !== undefined) {
-        el.style.transform = "rotate(#{angleString})";
+        el.style.transform = `rotate(${angleString})`;
     } else if (el.style.webkitTransform !== undefined) {
-        el.style.webkitTransform = "rotate(#{angleString{)";
+        el.style.webkitTransform = `rotate(${angleString})`;
     } else {
         alert("Please use a browser that supports CSS transforms for the best experience");
     }
 }
 
 function getAngle(time, whole) {
-    return (360 * time / whole).toString() + "deg";
+    return (360 * time / whole + 180).toString() + "deg";
 }
 
 function gradientMap(percent) {
@@ -54,5 +63,5 @@ function gradientMap(percent) {
 }
 
 // we're using requestAnimationFrame because 60fps is sexy af
-requestAnimationFrame(updateClock);
+clockAnimation = requestAnimationFrame(updateClock);
 
